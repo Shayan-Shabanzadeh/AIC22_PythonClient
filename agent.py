94e0e824101191e19d6560d359da9aa4463317b5
Shayan Shabanzadeh <shayanshabanzadeh0@gmail.com>
2022-05-21 19:21:40 +0430

    add: move and chat ai for thief and police

--- agent.py
+++ agent.py
@@ -1,6 +1,26 @@
-from stubs.v1.hide_and_seek_pb2 import GameView, DoActionCommand
+from hide_and_seek_pb2 import WatchCommand, GameView, MoveCommand, ChatCommand
 
 
-def ai(view: GameView) -> DoActionCommand:
-    # TODO: implement this method
+def get_thief_starting_node(watch: WatchCommand) -> int:
+    # TODO
+    return 2
+
+
+def thief_move_ai(view: GameView) -> MoveCommand:
+    # TODO
+    pass
+
+
+def police_move_ai(view: GameView) -> MoveCommand:
+    # TODO
+    pass
+
+
+def thief_chat_ai(view: GameView) -> ChatCommand:
+    # TODO
+    pass
+
+
+def police_chat_ai(view: GameView) -> ChatCommand:
+    # TODO
     pass
