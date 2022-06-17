d0012ba14bca1e87e015930d1fd0f6f558cf80d9
shayan <shayanshabanzadeh0@gmail.com>
2022-06-17 23:27:57 +0430

    fix: some bugs

--- agent.py
+++ agent.py
@@ -1,26 +1,34 @@
-from hide_and_seek_pb2 import WatchCommand, GameView, MoveCommand, ChatCommand
+import yaml
+
+from hide_and_seek_pb2 import GameView, MoveCommand, ChatCommand
+
+with open("./config/application.yml", "r") as config_file:
+    cfg = yaml.load(config_file, Loader=yaml.FullLoader)
 
 
-def get_thief_starting_node(watch: WatchCommand) -> int:
+def get_thief_starting_node(view: GameView) -> int:
     # TODO
     return 2
 
 
-def thief_move_ai(view: GameView) -> MoveCommand:
+
+def thief_move_ai(view: GameView) -> int:
+    # TODO
+    return 2
+
+
+def police_move_ai(view: GameView) -> int:
+    # TODO
+    return 1
+
+
+def thief_chat_ai(view: GameView) -> int:
     # TODO
     pass
 
 
-def police_move_ai(view: GameView) -> MoveCommand:
+def police_chat_ai(view: GameView) -> int:
     # TODO
     pass
 
 
-def thief_chat_ai(view: GameView) -> ChatCommand:
-    # TODO
-    pass
-
-
-def police_chat_ai(view: GameView) -> ChatCommand:
-    # TODO
-    pass
--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from concurrent.futures import ThreadPoolExecutor
 
 import grpc
@@ -10,13 +11,16 @@ from hide_and_seek_pb2 import *
 
 
 class GameClient:
-    __slots__ = ('token', 'channel', 'stub', 'server_address', 'ai_move_method', 'ai_chat_method')
+    __slots__ = (
+        'token', 'channel', 'has_moved', 'turn_number', 'stub', 'server_address', 'ai_move_method', 'ai_chat_method')
 
     def __init__(self, token: str, server_address: str) -> None:
         self.server_address = server_address
         self.channel = grpc.insecure_channel(self.server_address)
         self.stub = hide_and_seek_pb2_grpc.GameHandlerStub(channel=self.channel)
         self.token = token
+        self.has_moved = False
+        self.turn_number = 1
         self.ai_move_method = None
         self.ai_chat_method = None
 
@@ -25,29 +29,42 @@ class GameClient:
         watch_command = WatchCommand(token=self.token)
         try:
             for view in self.stub.Watch(watch_command):
+                self.check_and_end_the_game(view)
+                if view.turn.turnNumber != self.turn_number:
+                    self.turn_number = view.turn.turnNumber
+                    self.has_moved = False
                 logging.info(view)
-                game_status = view.status
                 index += 1
                 if index == 1:
                     print(self.token)
                     try:
                         self.stub.DeclareReadiness(self.get_join_game_command(view))
-                    except Exception as e:
-                        print(e)
+                    except Exception:
+                        print(traceback.format_exc())
                     self.set_ai_methods(view)
-                elif game_status == GameStatus.ONGOING:
+                elif self.check_if_is_client_turn_to_move(view):
                     try:
                         self.send_message(view)
                         self.move(view)
-                    except Exception as e:
-                        print(e)
-                elif game_status == GameStatus.FINISHED:
-                    self.channel.unsubscribe(lambda _: None)
-
-        except Exception as e:
-            print(e)
+                        self.has_moved = True
+                    except Exception:
+                        print(traceback.format_exc())
+        except Exception:
             self.channel.unsubscribe()
             exit()
+
+    def check_and_end_the_game(self, view):
+        if view.status == GameStatus.FINISHED:
+            self.channel.unsubscribe(lambda _: None)
+
+    def check_if_is_client_turn_to_move(self, view: GameView):
+        if view.status == GameStatus.ONGOING and not self.has_moved:
+            if view.turn.turnType == TurnType.THIEF_TURN and view.viewer.type == AgentType.THIEF:
+                return True
+            if view.turn.turnType == TurnType.POLICE_TURN and view.viewer.type == AgentType.POLICE:
+                return True
+            return False
+        return False
 
     def send_message(self, view):
         chat_command = self.ai_chat_method(view)
@@ -55,7 +72,8 @@ class GameClient:
             self.stub.SendMessage(chat_command)
 
     def move(self, view):
-        move_command = self.ai_move_method(view)
+        node_id = self.ai_move_method(view)
+        move_command = MoveCommand(token=self.token, toNodeId=node_id)
         if move_command:
             self.stub.Move(move_command)
 
@@ -66,7 +84,7 @@ class GameClient:
             start_node_id = agent.get_thief_starting_node(view)
             return DeclareReadinessCommand(token=self.token, startNodeId=start_node_id)
         else:
-            return DeclareReadinessCommand(token=self.token, startNodeId=2)
+            return DeclareReadinessCommand(token=self.token, startNodeId=1)
 
     def set_ai_methods(self, view: GameView) -> None:
         viewer_type = view.viewer.type
